23b19d80d3afdaffa08093b8415d967984d35ffd
IidaPeltonen <n0peii00@students.oamk.fi>
2021-09-29 13:03:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import {useState} from 'react';
+
+const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=';
+const API_KEY = '43bace85d342e898e170cd24ffa00b6f';
+
+function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+
+  return (
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <label>Eurot </label>&nbsp;
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)} />
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Punnat </label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+        <div>
+          <button>Laske</button>
+        </div>
+      </form>
+    </div>
+  );
+
+  async function convert(e) {
+    e.preventDefault();
+    try {
+      const address = URL + API_KEY;
+      const response = await fetch(address);
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json.rates.GBP);
+        setRate(json.rates.GBP);
+
+        setGbp(eur * json.rates.GBP);
+      }
+      else {
+        alert('Virhe valuutta-arvoa haettaessa!');
+        console.log(response);
+      }
+    }
+    catch (err) {
+      alert(err);
+    }
+  }
+}
+export default App;
